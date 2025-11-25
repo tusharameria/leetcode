ff4ec0e1619f93598bfb5c5b84c6bd63e2fed4d3
Tushar Ameria <tusharameria@gmail.com>
2025-11-25 13:05:38 +0530

    23. Merge k sorted lists

--- src/problems/problem_23.go
+++ src/problems/problem_23.go
@@ -8,5 +8,29 @@ func Problem_23() {
 }
 
 func mergeKLists(lists []*ListNode) *ListNode {
-	return nil
+	n := len(lists)
+	if n == 0 {
+		return nil
+	}
+	ans := lists
+
+	for len(ans) > 1 {
+		ans = mergeProcess(ans)
+	}
+	return ans[0]
+}
+
+func mergeProcess(lists []*ListNode) []*ListNode {
+	res := []*ListNode{}
+	n := len(lists)
+	i := 0
+	if n%2 == 1 {
+		res = append(res, lists[0])
+		i = 1
+	}
+	for i <= len(lists)-1 {
+		res = append(res, mergeTwoListsEff(lists[i], lists[i+1]))
+		i += 2
+	}
+	return res
 }
