5693b52c9c72ebb5482af9127746a4c88faf64fa
Tushar Ameria <tusharameria@gmail.com>
2026-02-03 16:33:47 +0530

    knapsack 0/1 - dp

--- src/problems/knapsack_0_1.go
+++ src/problems/knapsack_0_1.go
@@ -8,23 +8,31 @@ func Problem_knapsack_0_1() {
 	weights := []int{2, 1, 3, 2, 4, 5}
 	values := []int{12, 10, 20, 15, 25, 30}
 	capacity := 7
-	fmt.Printf("res : %v\n", knap01(weights, values, capacity, 0))
+	fmt.Printf("res : %v\n", knap01(weights, values, capacity))
 }
 
-func knap01(weights, values []int, capacity, i int) int {
-	if i == len(weights) || capacity <= 0 {
-		return 0
+func knap01(weights, values []int, capacity int) int {
+	dp := make([][]int, len(weights))
+	for i := 0; i <= len(dp)-1; i++ {
+		dp[i] = make([]int, capacity+1)
 	}
-
-	skip := knap01(weights, values, capacity, i+1)
-	take := 0
-
-	if weights[i] <= capacity {
-		take = values[i] + knap01(weights, values, capacity-weights[i], i+1)
+	for i := len(dp) - 1; i >= 0; i-- {
+		for j := 1; j <= capacity; j++ {
+			inc := 0
+			if weights[i] <= j {
+				inc = values[i]
+			}
+			if j-weights[i] >= 0 {
+				if i+1 < len(dp) {
+					inc += dp[i+1][j-weights[i]]
+				}
+			}
+			exc := 0
+			if i+1 < len(dp) {
+				exc = dp[i+1][j]
+			}
+			dp[i][j] = max(inc, exc)
+		}
 	}
-
-	if take > skip {
-		return take
-	}
-	return skip
+	return dp[0][capacity]
 }
