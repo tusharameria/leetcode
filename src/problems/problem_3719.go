33448a6684f6d4593d02a04cc2d0ebf03d69954f
Tushar Ameria <tusharameria@gmail.com>
2026-02-10 14:07:45 +0530

    3719 - opt

--- src/problems/problem_3719.go
+++ src/problems/problem_3719.go
@@ -1,25 +1,31 @@
 package problems
 
+import "fmt"
+
 // 3719. Longest Balanced Subarray I
 
 func Problem_3719() {
-	nums := []int{1, 2, 3, 4, 5, 6, 6}
-	println(longestBalanced(nums))
+	nums := []int{1, 2, 3, 2, 5}
+	fmt.Println(longestBalanced(nums))
 }
 
 func longestBalanced(nums []int) int {
 	maxi := 0
-	for i := 0; i <= len(nums)-1; i++ {
-		counts := make(map[int]int)
-		counts[nums[i]]++
-		distinctCounts := make(map[int]int)
-		distinctCounts[nums[i]%2]++
+	for i := 0; i <= len(nums)-1-maxi; i++ {
+		evenSet := make(map[int]bool)
+		oddSet := make(map[int]bool)
+		if nums[i]%2 == 0 {
+			evenSet[nums[i]] = true
+		} else {
+			oddSet[nums[i]] = true
+		}
 		for j := i + 1; j <= len(nums)-1; j++ {
-			if _, ok := counts[nums[j]]; !ok {
-				distinctCounts[nums[j]%2]++
+			if nums[j]%2 == 0 {
+				evenSet[nums[j]] = true
+			} else {
+				oddSet[nums[j]] = true
 			}
-			counts[nums[j]]++
-			if distinctCounts[0] == distinctCounts[1] {
+			if len(evenSet) == len(oddSet) {
 				maxi = max(maxi, j-i+1)
 			}
 		}
