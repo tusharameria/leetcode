453158c758320fa2b845e05630a00e36bf46aa3e
Tushar Ameria <tusharameria@gmail.com>
2026-02-12 18:52:18 +0530

    36

--- src/problems/problem_36.go
+++ src/problems/problem_36.go
@@ -14,16 +14,44 @@ func Problem_36() {
 		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
 		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
 		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
-		{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
-	}
+		{'.', '.', '.', '.', '8', '.', '.', '7', '9'}}
 
 	fmt.Println("res : ", isValidSudoku(board))
 }
 
 func isValidSudoku(board [][]byte) bool {
-	dot := byte('.')
-	zero := byte('0')
-	fmt.Println("dot : ", dot)
-	fmt.Println("zero : ", zero)
+
+	for i := 0; i < 9; i++ {
+		rowSet := make(map[byte]int)
+		colSet := make(map[byte]int)
+		boxSet := make(map[byte]int)
+		for j := 0; j < 9; j++ {
+			row := i
+			col := j
+			if board[row][col] != '.' {
+				rowSet[board[row][col]]++
+				if rowSet[board[row][col]] > 1 {
+					return false
+				}
+			}
+			row = j
+			col = i
+			if board[row][col] != '.' {
+				colSet[board[row][col]]++
+				if colSet[board[row][col]] > 1 {
+					return false
+				}
+			}
+			row = (i/3)*3 + (j / 3)
+			col = (i%3)*3 + (j % 3)
+			if board[row][col] != '.' {
+				boxSet[board[row][col]]++
+				if boxSet[board[row][col]] > 1 {
+					return false
+				}
+			}
+		}
+	}
+
 	return true
 }
