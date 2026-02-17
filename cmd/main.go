bd2eaa7284a33b9f65e5ac85dbbac2638837a195
Tushar Ameria <tusharameria@gmail.com>
2026-02-17 13:59:44 +0530

    1200 - opt

--- cmd/main.go
+++ cmd/main.go
@@ -8,5 +8,5 @@ import (
 
 func main() {
 	fmt.Println("Hello World!")
-	problems.Problem_190()
+	problems.Problem_1200()
 }
--- src/problems/problem_1200.go
+++ src/problems/problem_1200.go
@@ -2,31 +2,53 @@ package problems
 
 import (
 	"fmt"
-	"sort"
 )
 
 // 1200. Minimum Absolute Difference
 
 func Problem_1200() {
-	arr := []int{4, 2, 1, 3}
+	// arr := []int{4, 2, 1, 3}
 	// arr := []int{1, 3, 6, 10, 15}
-	// arr := []int{3, 8, -10, 23, 19, -4, -14, 27}
+	arr := []int{3, 8, -10, 23, 19, -4, -14, 27}
 	fmt.Println("res : ", minimumAbsDifference(arr))
 }
 
 func minimumAbsDifference(arr []int) [][]int {
-	sort.Ints(arr)
-	res := make([][]int, 0)
-	min := arr[1] - arr[0]
-	res = append(res, []int{arr[0], arr[1]})
-	for i := 2; i <= len(arr)-1; i++ {
-		diff := arr[i] - arr[i-1]
-		if diff == min {
-			res = append(res, []int{arr[i-1], arr[i]})
-		} else if diff < min {
-			min = diff
-			res = make([][]int, 0)
-			res = append(res, []int{arr[i-1], arr[i]})
+	n := len(arr)
+
+	// Find min and max values in the original array
+	minVal := arr[0]
+	maxVal := arr[0]
+	for i := 0; i <= n-1; i++ {
+		minVal = min(minVal, arr[i])
+		maxVal = max(maxVal, arr[i])
+	}
+
+	// Length of dp array
+	m := maxVal - minVal + 1
+	dp := make([]bool, m)
+
+	// Initialise dp array
+	for i := 0; i <= n-1; i++ {
+		dp[arr[i]-minVal] = true
+
+	}
+
+	res := make([][]int, 0, n)
+
+	l := 0
+	minDiff := m - 1
+
+	for r := 1; r <= m-1; r++ {
+		if dp[r] {
+			if r-l <= minDiff {
+				if r-l < minDiff {
+					minDiff = r - l
+					res = res[:0] // Clear the result array
+				}
+				res = append(res, []int{l + minVal, r + minVal})
+			}
+			l = r
 		}
 	}
 	return res
