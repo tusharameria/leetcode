e3e3a4c0410eb588ba55d655e59d2a919a8881a3
Tushar Ameria <tusharameria@gmail.com>
2026-03-11 20:09:46 +0530

    1680 - opt

--- src/problems/problem_1680.go
+++ src/problems/problem_1680.go
@@ -8,19 +8,14 @@ func Problem_1680() {
 }
 
 func concatenatedBinary(n int) int {
-	remNum := 1000000007
-	rem := 1
-	for i := 2; i <= n; i++ {
-		rem = ((rem * PowInt(2, noBinaryDigits(i))) + i) % remNum
+	mod := 1000000007
+	result := 0
+	length := 0
+	for i := 1; i <= n; i++ {
+		if (i & (i - 1)) == 0 {
+			length++
+		}
+		result = ((result << length) | i) % mod
 	}
-	return rem
-}
-
-func noBinaryDigits(n int) int {
-	res := 0
-	for n > 0 {
-		res++
-		n /= 2
-	}
-	return res
+	return result
 }
