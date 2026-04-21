5cbc47c05856b2cbf0690246b3393060ece3b054
Tushar Ameria <tusharameria@gmail.com>
2026-04-21 23:03:28 +0530

    3741

--- src/problems/problem_3740.go
+++ src/problems/problem_3740.go
@@ -2,7 +2,14 @@
 
 package problems
 
-func minimumDistance(nums []int) int {
+import "fmt"
+
+func Problem_3740() {
+	nums := []int{1, 2, 3, 4, 5, 1, 2, 3, 4, 5}
+	fmt.Println(minimumDistanceOld(nums))
+}
+
+func minimumDistanceOld(nums []int) int {
 
 	distance := 2*(len(nums)-1) + 1
 	tupleMap := make(map[int][]int)
---
+++ src/problems/problem_3741.go
@@ -0,0 +1,28 @@
+// 3741. Minimum Distance Between Three Equal Elements II
+
+package problems
+
+import "fmt"
+
+func Problem_3741() {
+	nums := []int{1, 2, 3, 4, 5, 1, 2, 3, 4, 5}
+	fmt.Println(minimumDistance(nums))
+}
+
+func minimumDistance(nums []int) int {
+	numsLen := len(nums)
+	distance := 2*(numsLen-1) + 1
+	tupleArray := make([][]int, numsLen+1)
+	for i, num := range nums {
+		tupleArray[num] = append(tupleArray[num], i)
+		indicesLen := len(tupleArray[num])
+		if indicesLen >= 3 {
+			distance = min(distance, 2*(tupleArray[num][indicesLen-1]-tupleArray[num][indicesLen-3]))
+		}
+	}
+
+	if distance == 2*(numsLen-1)+1 {
+		return -1
+	}
+	return distance
+}
