2763291b9fb4090d0c22e5e9ec35c6eb631439bf
Tushar Ameria <tusharameria@gmail.com>
2026-04-23 19:46:47 +0530

    2615 - bit cleaner

--- src/problems/problem_2615.go
+++ src/problems/problem_2615.go
@@ -12,31 +12,27 @@ func Problem_2615() {
 func distance(nums []int) []int64 {
 	numLen := len(nums)
 	result := make([]int64, numLen)
-	indexSumMap := make(map[int][]int64)
-	// indexMapIndex := make(map[int]map[int]int)
+	indexMap := make(map[int][]int64)
 
 	for i := 0; i <= numLen-1; i++ {
-		indexSumMapLen := len(indexSumMap[nums[i]])
-		if indexSumMapLen == 0 {
-			indexSumMap[nums[i]] = append(indexSumMap[nums[i]], int64(i))
-		} else {
-			indexSumMap[nums[i]] = append(indexSumMap[nums[i]], indexSumMap[nums[i]][indexSumMapLen-1]+int64(i))
-		}
+		indexMap[nums[i]] = append(indexMap[nums[i]], int64(i))
 	}
 
-	for i := range indexSumMap {
-		indexSumMapLen := len(indexSumMap[i])
-		if indexSumMapLen > 1 {
+	for i := range indexMap {
+		indexMapLen := len(indexMap[i])
+		if indexMapLen > 1 {
 			leftSum := int64(0)
-			for j := 0; j <= indexSumMapLen-1; j++ {
-				currentVal := indexSumMap[i][j] - leftSum
-				leftSum += currentVal
+			totalSum := int64(0)
+			for j := 0; j <= indexMapLen-1; j++ {
+				totalSum += indexMap[i][j]
+			}
+			for j := 0; j <= indexMapLen-1; j++ {
+				currentVal := indexMap[i][j]
 				resultIndex := currentVal
-				totalSum := indexSumMap[i][indexSumMapLen-1]
-				currentSum := indexSumMap[i][j]
-				forwardSum := totalSum - currentSum - currentVal*int64(indexSumMapLen-1-j)
-				backwardSum := currentVal*int64(j+1) - currentSum
+				forwardSum := totalSum - leftSum - currentVal*int64(indexMapLen-j)
+				backwardSum := currentVal*int64(j) - leftSum
 				result[resultIndex] = forwardSum + backwardSum
+				leftSum += currentVal
 			}
 		}
 	}
