46afb35350544b17456ec2000eb420aaef75646b
Tushar Ameria <tusharameria@gmail.com>
2026-04-24 18:19:35 +0530

    2069 - better but still unopt

--- src/problems/problem_2069.go
+++ src/problems/problem_2069.go
@@ -2,6 +2,27 @@
 
 package problems
 
+import "fmt"
+
+func Problem_2069() {
+	robot := Constructor(6, 3)
+	fmt.Printf("Pos : %v, Dir : %s\n", robot.GetPos(), robot.GetDir())
+	commands := []string{"step", "step", "getPos", "getDir", "step", "step", "step", "getPos", "getDir"}
+	inputs := []int{2, 2, 0, 0, 2, 1, 4, 0, 0}
+	for i := range commands {
+		if commands[i] == "step" {
+			robot.Step(inputs[i])
+			fmt.Printf("Step : %d, Pos : %v, Dir : %s\n", inputs[i], robot.GetPos(), robot.GetDir())
+		} else if commands[i] == "getPos" {
+			robot.GetPos()
+			fmt.Printf("Pos : %v\n", robot.GetPos())
+		} else if commands[i] == "getDir" {
+			robot.GetDir()
+			fmt.Printf("Dir : %s\n", robot.GetDir())
+		}
+	}
+}
+
 type Robot struct {
 	X int
 	Y int
@@ -13,8 +34,8 @@ type Robot struct {
 
 func Constructor(width int, height int) Robot {
 	return Robot{
-		X:         2,
-		Y:         9,
+		X:         0,
+		Y:         0,
 		Direction: 0,
 		Width:     width,
 		Height:    height,
@@ -48,39 +69,48 @@ func (this *Robot) Step(num int) {
 			// Moving in X direction
 			if div == 0 {
 				// Moving in positive X direction
-				if this.X+1 == this.Width {
+				if num > this.Width-1-this.X {
+					num -= this.Width - 1 - this.X
+					this.X = this.Width - 1
 					this.Direction = (this.Direction + 1) % 4
+					fmt.Printf("num : %d, X : %d, Direction : %d\n", num, this.X, this.Direction)
 					continue
 				}
-				this.X++
-				num--
+				this.X += num
+				return
 			} else {
 				// Moving in negative X direction
-				if this.X == 0 {
+				if num > this.X {
+					num -= this.X
+					this.X = 0
 					this.Direction = (this.Direction + 1) % 4
 					continue
 				}
-				this.X--
-				num--
+				this.X -= num
+				return
 			}
 		} else {
 			// Moving in Y direction
 			if div == 0 {
 				// Moving in positive Y direction
-				if this.Y+1 == this.Height {
+				if num > this.Height-1-this.Y {
+					num -= this.Height - 1 - this.Y
+					this.Y = this.Height - 1
 					this.Direction = (this.Direction + 1) % 4
 					continue
 				}
-				this.Y++
-				num--
+				this.Y += num
+				return
 			} else {
 				// Moving in negative Y direction
-				if this.Y == 0 {
+				if num > this.Y {
+					num -= this.Y
+					this.Y = 0
 					this.Direction = (this.Direction + 1) % 4
 					continue
 				}
-				this.Y--
-				num--
+				this.Y -= num
+				return
 			}
 		}
 	}
