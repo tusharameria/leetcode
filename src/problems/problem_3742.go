6efb3fc8b3154b8b8e5a4becc67df7b9e62c0f60
Tushar Ameria <tusharameria@gmail.com>
2026-05-05 22:41:55 +0530

    3742 - opt

--- src/problems/problem_3742.go
+++ src/problems/problem_3742.go
@@ -2,61 +2,56 @@
 
 package problems
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func Problem_3742() {
-	grid := [][]int{{1, 2}, {0, 2}}
-	fmt.Println(maximumPathScore(grid, 2))
+	grid := [][]int{{0, 1, 2}, {0, 1, 1}, {2, 0, 2}}
+	fmt.Println(maxPathScore(grid, 2))
 }
 
-func maximumPathScore(grid [][]int, k int) int {
+func maxPathScore(grid [][]int, k int) int {
 	m := len(grid)
 	n := len(grid[0])
-	dp := make([][][]int, m+1)
+	var dp [201][400]int
 	INFINT := 1 << 30
-	for i := 0; i < m+1; i++ {
-		dp[i] = make([][]int, n+1)
-		for j := 0; j < n+1; j++ {
-			dp[i][j] = make([]int, k+1)
-			for l := 0; l < k+1; l++ {
-				dp[i][j][l] = -INFINT
-			}
+
+	// Check if reaching end tile is possible
+	firstRow := dp[0]
+	for j, val := range grid[0] {
+		firstRow[j+1] = firstRow[j] + min(val, 1)
+	}
+	firstRow[0] = INFINT
+	for i := 1; i < m; i++ {
+		for j, val := range grid[i] {
+			firstRow[j+1] = min(firstRow[j], firstRow[j+1]) + min(val, 1)
 		}
 	}
-	for i := 1; i <= m; i++ {
-		for j := 1; j <= n; j++ {
-			for l := 0; l <= k; l++ {
-				if i == 1 && j == 1 {
-					dp[i][j][l] = 0
-				} else {
-					score := grid[i-1][j-1]
-					cost := (score + 1) / 2
-					if l-cost < 0 {
-						dp[i][j][l] = -INFINT
-						continue
-					}
-					upperScore := dp[i-1][j][l-cost]
-					leftScore := dp[i][j-1][l-cost]
-					maxScore := max(upperScore, leftScore)
-					if maxScore == -INFINT {
-						dp[i][j][l] = -INFINT
-						continue
-					}
-					dp[i][j][l] = maxScore + score
-				}
-			}
-		}
-	}
-	finalMaxScore := -INFINT
-	for i := k; i >= 0; i-- {
-		curr := dp[m][n][i]
-		if curr == -INFINT {
-			break
-		}
-		finalMaxScore = max(curr, finalMaxScore)
-	}
-	if finalMaxScore == -INFINT {
+
+	if firstRow[n] > k {
 		return -1
 	}
-	return finalMaxScore
+
+	// Max Score
+	for j := range n + 1 {
+		for c := range min(k+2, m+n) {
+			dp[j][c] = -INFINT
+		}
+	}
+
+	dp[1][1] = 0
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			val := grid[i][j]
+			step := 1 - min(1, val)
+			for c := min(k+2, i+j); c >= 0; c-- {
+				dp[j+1][c+1] = max(dp[j+1][c+step], dp[j][c+step]) + val
+			}
+		}
+	}
+
+	return slices.Max(dp[n][:min(k+2, m+n)])
 }
