2758ce11a85e7a75a622a55463e94297dcf3320a
Tushar Ameria <tusharameria@gmail.com>
2026-05-06 20:44:15 +0530

    3653 - little unopt

---
+++ src/problems/problem_3653.go
@@ -0,0 +1,27 @@
+// 3653. XOR After Range Multiplication Queries I
+
+package problems
+
+func Problem_3653() {
+	nums := []int{2, 3, 1, 5, 4}
+	queries := [][]int{
+		{1, 4, 2, 3},
+		{0, 2, 1, 2},
+	}
+	println(xorAfterQueries(nums, queries))
+}
+
+func xorAfterQueries(nums []int, queries [][]int) int {
+	res := 0
+	divider := 1000000007
+	for i := range queries {
+		l, r, step, multiplier := queries[i][0], queries[i][1], queries[i][2], queries[i][3]
+		for j := l; j <= r; j += step {
+			nums[j] = (nums[j] * multiplier) % divider
+		}
+	}
+	for _, v := range nums {
+		res ^= v
+	}
+	return res
+}
