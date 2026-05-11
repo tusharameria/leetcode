bfa7e3cfce9923f7d4bd5f6330471dda7f49ec2a
Tushar Ameria <tusharameria@gmail.com>
2026-05-11 21:14:53 +0530

    1914 - opt

--- src/problems/problem_1914.go
+++ src/problems/problem_1914.go
@@ -18,60 +18,36 @@ func Problem_1914() {
 func rotateGrid(grid [][]int, k int) [][]int {
 	height := len(grid)
 	width := len(grid[0])
-	res := make([][]int, height)
-	for i := 0; i < height; i++ {
-		res[i] = make([]int, width)
-		copy(res[i], grid[i])
-	}
 	layers := min(height, width) / 2
 	for layer := 0; layer < layers; layer++ {
-		// fmt.Printf("Layer : %d\n", layer)
-		layerHeight := height - 2*layer
-		layerWidth := width - 2*layer
-		layerElements := 2 * (layerHeight + layerWidth - 2)
-		// fmt.Printf("layerHeight : %d\n", layerHeight)
-		// fmt.Printf("layerWidth : %d\n", layerWidth)
-		// fmt.Printf("layerElements : %d\n", layerElements)
-		shift := k % layerElements
-		// fmt.Printf("Shift : %d\n", shift)
-		if shift != 0 {
-			// shift range => [1, layerElements-1]
-			for count := 0; count < layerElements; count++ {
-				nextCount := (count + layerElements - shift) % layerElements
-				currentRow := 0
-				currentCol := 0
-				if count < layerWidth {
-					currentCol = count
-				} else if count < layerWidth+layerHeight-1 {
-					currentRow = count - layerWidth + 1
-					currentCol = layerWidth - 1
-				} else if count < 2*layerWidth+layerHeight-2 {
-					currentRow = layerHeight - 1
-					currentCol = 2*layerWidth + layerHeight - 3 - count
-				} else {
-					currentRow = layerHeight - 1 - (count - (2*layerWidth + layerHeight - 3))
-				}
-				currentRow += layer
-				currentCol += layer
-				// For next element
-				nextRow := 0
-				nextCol := 0
-				if nextCount < layerWidth {
-					nextCol = nextCount
-				} else if nextCount < layerWidth+layerHeight-1 {
-					nextRow = nextCount - layerWidth + 1
-					nextCol = layerWidth - 1
-				} else if nextCount < 2*layerWidth+layerHeight-2 {
-					nextRow = layerHeight - 1
-					nextCol = 2*layerWidth + layerHeight - 3 - nextCount
-				} else {
-					nextRow = layerHeight - 1 - (nextCount - (2*layerWidth + layerHeight - 3))
-				}
-				nextRow += layer
-				nextCol += layer
-				res[nextRow][nextCol] = grid[currentRow][currentCol]
-			}
+		rowIndex := []int{}
+		colIndex := []int{}
+		values := []int{}
+		for i := layer; i <= width-layer-2; i++ {
+			rowIndex = append(rowIndex, layer)
+			colIndex = append(colIndex, i)
+			values = append(values, grid[layer][i])
+		}
+		for i := layer; i <= height-layer-2; i++ {
+			rowIndex = append(rowIndex, i)
+			colIndex = append(colIndex, width-layer-1)
+			values = append(values, grid[i][width-layer-1])
+		}
+		for i := width - layer - 1; i >= layer+1; i-- {
+			rowIndex = append(rowIndex, height-layer-1)
+			colIndex = append(colIndex, i)
+			values = append(values, grid[height-layer-1][i])
+		}
+		for i := height - layer - 1; i >= layer+1; i-- {
+			rowIndex = append(rowIndex, i)
+			colIndex = append(colIndex, layer)
+			values = append(values, grid[i][layer])
+		}
+		n := len(values)
+		for i := 0; i < n; i++ {
+			newIndex := (i + k%n) % n
+			grid[rowIndex[i]][colIndex[i]] = values[newIndex]
 		}
 	}
-	return res
+	return grid
 }
