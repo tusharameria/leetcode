813362412d3947689b62c5191e5a87c5d3f07bfd
Tushar Ameria <tusharameria@gmail.com>
2026-05-18 19:41:19 +0530

    1306 - opt

--- src/problems/problem_1306.go
+++ src/problems/problem_1306.go
@@ -11,52 +11,29 @@ func Problem_1306() {
 }
 
 func canReach(arr []int, start int) bool {
+	const SENTINEL = 99999999
 	n := len(arr)
-	store := make([][2]bool, n)
-
-	zero := false
-	for i := 0; i < n; i++ {
-		if arr[i] == 0 {
-			zero = true
-			break
-		}
-	}
-
-	if !zero {
-		return false
-	}
-
-	var iter func(index int) bool
-
-	iter = func(index int) bool {
+	store := make([]int, 1)
+	store[0] = start
+	for len(store) > 0 {
+		index := store[0]
 		val := arr[index]
 		if val == 0 {
 			return true
 		}
-		// both side already visited
-		if store[index][0] && store[index][1] {
-			return false
-		}
-		leftIndex := index - val
-		rightIndex := index + val
-		// both side out of bounds
-		if leftIndex < 0 && rightIndex >= n {
-			store[index][0] = true
-			store[index][1] = true
-			return false
-		}
-		leftPossible := !store[index][0] && leftIndex >= 0
-		rightPossible := !store[index][1] && rightIndex < n
-		store[index][0] = true
-		store[index][1] = true
-		if leftPossible && !rightPossible {
-			return iter(leftIndex)
-		} else if !leftPossible && rightPossible {
-			return iter(rightIndex)
-		} else {
-			return iter(leftIndex) || iter(rightIndex)
+		store = store[1:]
+		p := []int{index - val, index + val}
+		arr[index] = SENTINEL
+		for _, newIndex := range p {
+			if newIndex >= 0 && newIndex < n {
+				if arr[newIndex] == 0 {
+					return true
+				}
+				if arr[newIndex] != SENTINEL {
+					store = append(store, newIndex)
+				}
+			}
 		}
 	}
-
-	return iter(start)
+	return false
 }
