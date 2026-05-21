8dc16a25bcd3c796b6ddd93d0b3a3e8f24ea5052
Tushar Ameria <tusharameria@gmail.com>
2026-05-21 18:27:18 +0530

    3043 - not TLE but still unopt

--- src/problems/problem_3043.go
+++ src/problems/problem_3043.go
@@ -5,50 +5,60 @@ package problems
 import "fmt"
 
 func Problem_3043() {
-	arr1 := []int{5655359}
-	arr2 := []int{56554}
+	arr1 := []int{10}
+	arr2 := []int{17, 11}
 	fmt.Println(longestCommonPrefix(arr1, arr2))
 }
 
 func longestCommonPrefix(arr1 []int, arr2 []int) int {
-	maxVal := 0
 	len1 := len(arr1)
 	len2 := len(arr2)
-	firstArrStore := make([][]int, len1)
-	for i := 0; i < len1; i++ {
-		val := arr1[i]
-		buff := []int{}
-		for val > 0 {
-			buff = append(buff, val%10)
-			val /= 10
-		}
-		firstArrStore[i] = buff
-	}
-	secondArrStore := make([][]int, len2)
-	for i := 0; i < len2; i++ {
-		val := arr2[i]
-		buff := []int{}
-		for val > 0 {
-			buff = append(buff, val%10)
-			val /= 10
-		}
-		secondArrStore[i] = buff
-	}
-	for i := 0; i < len1; i++ {
-		for j := 0; j < len2; j++ {
-			maxVal = max(maxVal, getCommonPrefix(firstArrStore[i], secondArrStore[j]))
-		}
-	}
-	return maxVal
-}
 
-func getCommonPrefix(arr1, arr2 []int) int {
-	num := 0
+	store := make(map[int]bool)
+	maxVal := 0
+	if len1 < len2 {
+		for i := 0; i < len1; i++ {
+			val := arr1[i]
+			for val > 0 {
+				store[val] = true
+				val /= 10
+			}
+		}
 
-	for i, j := len(arr1)-1, len(arr2)-1; i >= 0 && j >= 0 && arr1[i] == arr2[j]; {
-		num++
-		i--
-		j--
+		for i := 0; i < len2; i++ {
+			val := arr2[i]
+			for val > 0 {
+				if store[val] {
+					maxVal = max(maxVal, val)
+				}
+				val /= 10
+			}
+		}
+	} else {
+		for i := 0; i < len2; i++ {
+			val := arr2[i]
+			for val > 0 {
+				store[val] = true
+				val /= 10
+			}
+		}
+
+		for i := 0; i < len1; i++ {
+			val := arr1[i]
+			for val > 0 {
+				if store[val] {
+					maxVal = max(maxVal, val)
+				}
+				val /= 10
+			}
+		}
 	}
-	return num
+
+	i := 0
+	for maxVal > 0 {
+		maxVal /= 10
+		i++
+	}
+
+	return i
 }
