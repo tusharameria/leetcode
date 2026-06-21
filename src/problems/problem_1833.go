c082ec44317461096d7763af56ac525a369aae3c
Tushar Ameria <tusharameria@gmail.com>
2026-06-22 00:04:24 +0530

    1833 - 11ms, 87%, better than last

--- src/problems/problem_1833.go
+++ src/problems/problem_1833.go
@@ -4,24 +4,55 @@ package problems
 
 import (
 	"fmt"
-	"sort"
 )
 
 func Problem_1833() {
-	costs := []int{1, 6, 3, 1, 2, 5}
+	costs := []int{1, 3, 2, 4, 1}
 	coins := 20
 	fmt.Println(maxIceCream(costs, coins))
 }
 
+// Constraints:
+
+// costs.length == n
+// 1 <= n <= 10^5
+// 1 <= costs[i] <= 10^5
+// 1 <= coins <= 10^8
+
 func maxIceCream(costs []int, coins int) int {
-	sort.Ints(costs)
-	count := 0
+	if len(costs) == 0 {
+		return 0
+	}
+	maxCost := 0
+	minCost := 1_00_001
 	for _, cost := range costs {
+		maxCost = max(maxCost, cost)
+		minCost = min(minCost, cost)
+	}
+	store := make([]int, maxCost-minCost+1)
+	for _, cost := range costs {
+		maxCost = max(maxCost, cost)
+		minCost = min(minCost, cost)
+	}
+	for _, cost := range costs {
+		store[cost-minCost]++
+	}
+	count := 0
+	for i, freq := range store {
+		cost := i + minCost
 		if coins < cost {
 			break
 		}
-		coins -= cost
-		count++
+		if freq != 0 {
+			for freq > 0 {
+				if coins < cost {
+					break
+				}
+				coins -= cost
+				count++
+				freq--
+			}
+		}
 	}
 	return count
 }
