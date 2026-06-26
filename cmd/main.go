c4633489bb0112dedd62b1b48fa282ec2b8a8af3
Tushar Ameria <tusharameria@gmail.com>
2026-06-26 21:18:06 +0530

    extra

--- cmd/main.go
+++ cmd/main.go
@@ -8,5 +8,21 @@ import (
 
 func main() {
 	fmt.Println("Hello World!")
-	problems.Problem_1404()
+	// nums := []int{1, 2, 3}
+	// fmt.Println(nums)
+	// nums = nums[1:]
+	// fmt.Println(nums)
+	// nums = nums[3:]
+	// fmt.Println(nums)
+
+	problems.Problem_3691()
+
+	// topksortedstructures.KTopPairSum()
+
+	// segmenttrees.GenerateSegmentTree()
+
+	// fmt.Println(problems.BinarySearchIntRange([]int{1, 1, 1, 1, 4}, 2))
+	// nums := []int{1, 5, 8, 12, 15, 18, 22, 25}
+	// fmt.Println(problems.LeftMostIndex(nums, 10))
+	// fmt.Println(problems.RightMostIndex(nums, 20))
 }
