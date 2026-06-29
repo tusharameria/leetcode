fbbf45e917323ed7682d3187ee34c1e072be3e0b
Tushar Ameria <tusharameria@gmail.com>
2026-06-29 19:07:40 +0530

    1846

---
+++ src/problems/problem_1846.go
@@ -0,0 +1,27 @@
+// 1846. Maximum Element After Decreasing and Rearranging
+
+package problems
+
+import (
+	"fmt"
+	"sort"
+)
+
+func Problem_1846() {
+	nums := []int{2, 2, 1, 2, 1}
+	fmt.Println(maximumElementAfterDecrementingAndRearranging(nums))
+}
+
+func maximumElementAfterDecrementingAndRearranging(arr []int) int {
+	sort.Ints(arr)
+	n := len(arr)
+	arr[0] = 1
+	for i := 1; i < n; i++ {
+		target := arr[i-1] + 1
+		if arr[i] > target {
+			arr[i] = target
+		}
+	}
+	fmt.Println(arr)
+	return arr[n-1]
+}
