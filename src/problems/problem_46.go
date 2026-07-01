21bce96a238acc6c83ab8603931cad7b0b4c3757
Tushar Ameria <tusharameria@gmail.com>
2026-07-01 22:03:18 +0530

    46 - bit unopt, 1ms, 5%

--- src/problems/problem_46.go
+++ src/problems/problem_46.go
@@ -5,21 +5,29 @@ package problems
 import "fmt"
 
 func Problem_46() {
-	nums := []int{1, 2, 3}
+	nums := []int{1, 1, 2}
 	fmt.Println(permute(nums))
 }
 
 func permute(nums []int) [][]int {
+	// n := len(nums)
+	// resLen := 1
+	// for i := 2; i <= n; i++ {
+	// 	resLen *= i
+	// }
 	res := [][]int{}
 
+	idx := 0
 	var fill func([]int, []int)
 	fill = func(initArray, leftArray []int) {
-		if len(leftArray) == 0 {
+		leftLen := len(leftArray)
+		if leftLen == 0 {
 			res = append(res, initArray)
+			idx++
 		}
-		for i := 0; i < len(leftArray); i++ {
+		for i := 0; i < leftLen; i++ {
 			buffInit := make([]int, len(initArray))
-			buffLeft := make([]int, len(leftArray))
+			buffLeft := make([]int, leftLen)
 			copy(buffInit, initArray)
 			copy(buffLeft, leftArray)
 			fill(append(buffInit, leftArray[i]), append(buffLeft[:i], buffLeft[i+1:]...))
