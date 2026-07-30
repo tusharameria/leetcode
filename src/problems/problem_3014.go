bb36327ac1322dd951f9c8e49d6ff83bc25b82dc
Tushar Ameria <tusharameria@gmail.com>
2026-07-30 15:23:50 +0530

    3014 - 0ms, 100%

---
+++ src/problems/problem_3014.go
@@ -0,0 +1,17 @@
+// 3014. Minimum Number of Pushes to Type Word I
+
+package problems
+
+import "fmt"
+
+func Problem_3014() {
+	word := "qwertyuiopasdfgh"
+	fmt.Println(minimumPushes(word))
+}
+
+func minimumPushes(word string) int {
+	n := len(word)
+	div := n / 8
+	rem := n % 8
+	return (4*div + rem) * (div + 1)
+}
