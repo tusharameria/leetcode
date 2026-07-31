c849dfbcc448f486318600c9edac4630f608be00
Tushar Ameria <tusharameria@gmail.com>
2026-07-31 15:19:30 +0530

    3016 - 2ms, 56% - dicey time calc by leetcode

--- src/problems/problem_3016.go
+++ src/problems/problem_3016.go
@@ -4,24 +4,34 @@ package problems
 
 import (
 	"fmt"
-	"sort"
 )
 
 func Problem_3016() {
-	word := "aabbccddeeffgghhiiiiii"
+	word := "abccdeggg"
 	fmt.Println(minimumPushes(word))
 }
 
 func minimumPushes(word string) int {
-	count := make([]int, 26)
+	count := make([]uint32, 26)
 	for _, ch := range word {
 		count[ch-'a']++
 	}
-	sort.Ints(count)
+	sort_3016(count)
 
 	res := 0
-	for i := 25; i >= 0 && count[i] != 0; i-- {
-		res += (((25 - i) / 8) + 1) * count[i]
+	for i, j := 0, 0; count[i] != 0; i++ {
+		if i%8 == 0 {
+			j++
+		}
+		res += int(count[i]) * j
 	}
 	return res
+}
+
+func sort_3016(arr []uint32) {
+	for i := 1; i < 26; i++ {
+		for j := i; j > 0 && arr[j] > arr[j-1]; j-- {
+			arr[j], arr[j-1] = arr[j-1], arr[j]
+		}
+	}
 }
