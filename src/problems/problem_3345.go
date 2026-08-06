f9df4944d0ab12dc95eba5d3b8cb8a51f8cf9f20
Tushar Ameria <tusharameria@gmail.com>
2026-08-06 16:01:24 +0530

    3345 - 0ms, 100%

---
+++ src/problems/problem_3345.go
@@ -0,0 +1,29 @@
+// 3345. Smallest Divisible Digit Product I
+
+package problems
+
+import "fmt"
+
+func Problem_3345() {
+	n, t := 11, 3
+	fmt.Println(smallestNumber(n, t))
+}
+
+func smallestNumber(n int, t int) int {
+	for i := n; ; i++ {
+		num := i / 10
+		digit := i % 10
+		prod := digit
+		for {
+			if num <= 0 {
+				if prod%t == 0 {
+					return i
+				}
+				break
+			}
+			digit = num % 10
+			num /= 10
+			prod *= digit
+		}
+	}
+}
